1b430d064f8d342bf23d11773dc7ea8c368537f5
Riley Knox <rileyknox2020@u.northwestern.edu>
2019-09-11 17:44:05 -0500

    Take player's input + housekeeping
    Ask player to choose a column in which to play
    Make sure the player enters an integer 1-7
    
    Define column heights at start of game
    
    Changed player B color from O to R

--- connectFour.py
+++ connectFour.py
@@ -5,8 +5,10 @@ from __future__ import print_function
 columns = [0,0,0,0,0,0,0]
 #define number of rows
 rows = 6
+#define height of each column
+columnHeights = [rows,rows,rows,rows,rows,rows,rows]
 #define player names and colors
-players = {'player1':['A','X'],'player2':['B','O']}
+players = {'player1':['A','X'],'player2':['B','R']}
 #start the game with zero turns played
 numTurns = 0
 
@@ -14,8 +16,24 @@ numTurns = 0
 for i in range(rows):
     print(columns)
 
+#play the game!
+while True:
 #determine who's turn it is
-if numTurns % 2 != 0:
-    player = players['player1'][0]
-else:
-    player = players['player2'][0]
+    if numTurns % 2 != 0:
+        player = players['player2'][0]
+    else:
+        player = players['player1'][0]
+#take player's input
+    while True:
+        #check to make sure it's an integer
+        while True:
+            try:
+                play = int(input('Player {}, it''s your turn! Pick a column: '.format(player)))
+                break
+            except NameError:
+                print('Enter an integer from 1 to 7!')
+        #check to make sure it's within the board
+        if play < 1 or play > 7:
+            print('Enter an integer from 1 to 7!')
+        else:
+            break
