7d4d16b6f9fac2f9eb63b0cecbccb8be745c23f7
Riley Knox <rileyknox2020@u.northwestern.edu>
2019-09-11 16:32:56 -0500

    Retrieve player data
    Defines number of turns played at game start (=0)
    Creates dictionary with player data (name & color)
    Determines who's turn it is based on the number of turns played.

--- connectFour.py
+++ connectFour.py
@@ -5,13 +5,17 @@ from __future__ import print_function
 columns = [0,0,0,0,0,0,0]
 #define number of rows
 rows = 6
+#define player names and colors
+players = {'player1':['A','X'],'player2':['B','O']}
+#start the game with zero turns played
+numTurns = 0
 
 #generate blank board
 for i in range(rows):
     print(columns)
 
-#class ConnectFour:
-#    if player = A
-#    color = X
-#    elif player = B
-#    color = O
+#determine who's turn it is
+if numTurns % 2 != 0:
+    player = players['player1'][0]
+else:
+    player = players['player2'][0]
