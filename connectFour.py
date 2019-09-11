eb8cb4006bd9e1d38b00b1f5079b739c9724c423
Riley Knox <rileyknox2020@u.northwestern.edu>
2019-09-11 15:55:35 -0500

    Initialize game
    Generates a blank 6x7 board using list and for loop

---
+++ connectFour.py
@@ -0,0 +1,17 @@
+#include python3 print function
+from __future__ import print_function
+
+#define each row as a list of zeros
+columns = [0,0,0,0,0,0,0]
+#define number of rows
+rows = 6
+
+#generate blank board
+for i in range(rows):
+    print(columns)
+
+#class ConnectFour:
+#    if player = A
+#    color = X
+#    elif player = B
+#    color = O
