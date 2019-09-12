452a5b182dc372b3ccc4303f4c46ce2e609ca90f
Riley Knox <rileyknox2020@u.northwestern.edu>
2019-09-12 00:27:12 -0500

    Use numpy to create board + finish gameplay
    Use numpy to create board as a 6x7 array of '0' strings
    Determine play position using playRow and playCol
    Place player 'piece' in lowest slot of chosen column
    Print new board
    Update height of chosen column
    Update number of turns
    
    GAME CAN BE PLAYED AS OF THIS COMMIT, HOWEVER NO WIN CHECKING

---
+++ connectFour-np.py
@@ -0,0 +1,62 @@
+#include python3 print function
+from __future__ import print_function
+
+#include numpy
+import numpy as np
+
+#define player names and colors
+players = {'player1':['A','X'],'player2':['B','R']}
+#define the board size
+rows = 6
+cols = 7
+#define the height of each column
+columnHeights = [rows,rows,rows,rows,rows,rows,rows]
+#start the game with zero turns played
+numTurns = 0
+
+#create board as 6x7 zeros array
+board = np.chararray((rows,cols))
+board[:] = 0
+print(board)
+
+#play the game!
+while True:
+
+    #determine who's turn it is
+    if numTurns % 2 != 0:
+        player = players['player2'][0]
+        playerID = players['player2'][1]
+    else:
+        player = players['player1'][0]
+        playerID = players['player1'][1]
+
+    #take player's input
+    while True:
+        #check to make sure it's an integer
+        while True:
+            try:
+                play = int(input("Player {}, it's your turn! Pick a column: ".format(player)))
+                break
+            except NameError:
+                print('Enter an integer from 1 to 7!')
+        #check to make sure it's within the board
+        if play < 1 or play > 7:
+            print('Enter an integer from 1 to 7!')
+        else:
+            break
+
+    #determine positioning of play
+    playCol = play-1
+    playRow = columnHeights[playCol]-1
+
+    #place player's color in lowest open column space
+    board[playRow,playCol] = playerID
+
+    #display the new board
+    print(board)
+
+    #update the column heights
+    columnHeights[playCol] = columnHeights[playCol]-1
+
+    #update the number of turns
+    numTurns += 1
